1a4f2ca2d9c0be36a0066efa963752736d04e362
Nouragami7 <https://github.com/Nouragami7>
2025-03-16 01:00:42 +0200

    get the current weather data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.1.10"
+    id("com.google.devtools.ksp")
+
 }
 
 android {
@@ -61,4 +64,29 @@ dependencies {
     implementation("com.exyte:animated-navigation-bar:1.0.0")
     //Lottie animation
     implementation ("com.airbnb.android:lottie-compose:6.2.0")
+
+    //Navigation host
+    implementation("androidx.navigation:navigation-compose:2.8.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose-android:2.8.7")
+//Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+//Room
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+// Kotlin Symbol Processing (KSP)
+    ksp("androidx.room:room-compiler:$room_version")
+// optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+//Glide
+    implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
+//LiveData & Compose
+    val compose_version = "1.0.0"
+    implementation("androidx.compose.runtime:runtime-livedata:$compose_version")
+
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
+
 }
--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -1,28 +1,28 @@
 package com.example.weatherapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.weatherapplication.navigation.SetupNavHost
+import com.example.weatherapplication.ui.theme.screens.splash.SplashScreen
+import com.example.weatherapplication.ui.viewmodel.WeatherViewModel
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
@@ -34,7 +34,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -46,193 +45,88 @@ import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
-import com.example.weatherapplication.ui.theme.screens.splash.SplashScreen
+import com.example.weatherapplication.navigation.NavigationManager
+import com.example.weatherapplication.navigation.ScreensRoute
+import com.example.weatherapplication.ui.theme.screens.splash.FavouriteScreen
+import com.example.weatherapplication.ui.theme.screens.splash.HomeScreen
+import com.example.weatherapplication.ui.theme.screens.splash.SearchScreen
+import com.example.weatherapplication.ui.theme.screens.splash.SettingsScreen
 import com.exyte.animatednavbar.AnimatedNavigationBar
 import com.exyte.animatednavbar.animation.balltrajectory.Parabolic
 import com.exyte.animatednavbar.animation.indendshape.Height
 import com.exyte.animatednavbar.animation.indendshape.shapeCornerRadius
-import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MainScreen()
-        }
-    }
-}
-
-@Composable
-fun MainScreen() {
-    var showSplash by remember { mutableStateOf(true) }
-
-    LaunchedEffect(Unit) {
-        delay(3000)
-        showSplash = false
-    }
-    if (showSplash) {
-        SplashScreen()
-    } else {
-        HomeScreenContent()
-    }
-
-
-}
-
-enum class NavigationBarItems(val icon: ImageVector) {
-    Home(icon = Icons.Default.Home),
-    Search(icon = Icons.Default.Search),
-    Settings(icon = Icons.Default.Settings),
-    Favourire(icon = Icons.Default.Favorite)
-}
-
-fun Modifier.noRippleClickable(onClick: () -> Unit): Modifier = composed {
-    clickable(indication = null, interactionSource = remember { MutableInteractionSource() }) {
-        onClick()
-    }
-}
-
-@Composable
-fun HomeScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Home Screen")
-    }
-}
-
-@Composable
-fun SearchScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Search Screen")
-    }
-}
-
-@Composable
-fun SettingsScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Settings Screen")
-    }
-}
-
-@Composable
-fun FavourireScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Favourire Screen")
-    }
-}
-
-@Composable
-fun HomeScreenContent() {
-    val navigationBarItems = NavigationBarItems.values()
-    var selectedIndex by remember { mutableStateOf(0) }
-    Scaffold(
-        modifier = Modifier
-            .padding(12.dp)
-            .padding(bottom = 56.dp),
-        bottomBar = {
-            AnimatedNavigationBar(
-                modifier = Modifier.height(64.dp),
-                selectedIndex = selectedIndex,
-                cornerRadius = shapeCornerRadius(cornerRadius = 34.dp),
-                ballAnimation = Parabolic(tween(300)),
-                indentAnimation = Height(tween(300)),
-                ballColor =  Color(0xFF41BDFC),
-                barColor =   Color(0xFF99CCFF),
-            ) {
-                navigationBarItems.forEach { item ->
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .noRippleClickable { selectedIndex = item.ordinal },
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Icon(
-                            modifier = Modifier.size(26.dp),
-                            imageVector = item.icon,
-                            contentDescription = item.name,
-                            tint = if (selectedIndex == item.ordinal)
-                                MaterialTheme.colorScheme.inversePrimary
-                            else
-                                MaterialTheme.colorScheme.onSurface
-                        )
+            var displaySplashScreen by remember { mutableStateOf(true) }
+            if (displaySplashScreen) {
+                SplashScreen {
+                    displaySplashScreen = false
+                }
+            } else {
+                Scaffold(
+                    modifier = Modifier.padding(bottom = 56.dp),
+                    bottomBar = { BottomNavigation() }
+                ) { padding ->
+                    Box(modifier = Modifier.padding(padding)) {
+                        SetupNavHost()
                     }
                 }
             }
         }
-    ) { innerPadding ->
-        Box(modifier = Modifier.padding(innerPadding)) {
-            when (navigationBarItems[selectedIndex]) {
-                NavigationBarItems.Home -> HomeScreen()
-                NavigationBarItems.Search -> SearchScreen()
-                NavigationBarItems.Settings -> SettingsScreen()
-                NavigationBarItems.Favourire -> SettingsScreen()
-            }
-        }
-    }
-    GetTheWeatherOfTheDays()
-
-}
-
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun GetTheWeatherOfTheDays() {
-    val pages = 5
-    val pagerState = rememberPagerState(
-        initialPage = 0,
-        initialPageOffsetFraction = 0f,
-        pageCount = { pages }
-    )
-
-    LaunchedEffect(Unit) {
-        pagerState.scrollToPage(0)
     }
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        HorizontalPager(
-            state = pagerState,
-            modifier = Modifier
-                .width(300.dp)
-                .height(400.dp)
-        ) { page ->
-            Card(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                shape = RoundedCornerShape(20.dp),
-                elevation = CardDefaults.cardElevation(12.dp)
-            ) {
+    @Composable
+    private fun BottomNavigation() {
+        val navigationBarItems = NavigationBarItems.values()
+        var selectedIndex by remember { mutableStateOf(0) }
+        AnimatedNavigationBar(
+            modifier = Modifier.height(64.dp),
+            selectedIndex = selectedIndex,
+            cornerRadius = shapeCornerRadius(cornerRadius = 34.dp),
+            ballAnimation = Parabolic(tween(300)),
+            indentAnimation = Height(tween(300)),
+            ballColor = Color(0xFF41BDFC),
+            barColor = Color(0xFF99CCFF),
+
+        ) {
+            navigationBarItems.forEachIndexed { index, item ->
                 Box(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .noRippleClickable {
+                            selectedIndex = index
+                            NavigationManager.navigateTo(item.route)
+                        },
                     contentAlignment = Alignment.Center
                 ) {
-                    Text(text = "Page $page")
+                    Icon(
+                        modifier = Modifier.size(26.dp),
+                        imageVector = item.icon,
+                        contentDescription = item.name,
+                        tint = if (selectedIndex == index)
+                            MaterialTheme.colorScheme.inversePrimary
+                        else
+                            MaterialTheme.colorScheme.onSurface
+                    )
                 }
             }
         }
+    }
 
-        Spacer(modifier = Modifier.height(16.dp))
+    enum class NavigationBarItems(val icon: ImageVector, val route: String) {
+        Home(icon = Icons.Default.Home, route = ScreensRoute.HomeScreen.route),
+        Search(icon = Icons.Default.Search, route = ScreensRoute.SearchScreen.route),
+        Settings(icon = Icons.Default.Settings, route = ScreensRoute.SettingsScreen.route),
+        Favourite(icon = Icons.Default.Favorite, route = ScreensRoute.FavouriteScreen.route)
+    }
 
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            repeat(pages) { index ->
-                val isSelected = pagerState.currentPage == index
-                Box(
-                    modifier = Modifier
-                        .size(if (isSelected) 12.dp else 8.dp)
-                        .padding(4.dp)
-                        .background(
-                            color = if (isSelected)  Color(0xFF45B3E7)
-                            else Color(0xFFCDEBF9),
-                            shape = CircleShape
-                        )
-                )
-            }
+    private fun Modifier.noRippleClickable(onClick: () -> Unit): Modifier = composed {
+        clickable(indication = null, interactionSource = remember { MutableInteractionSource() }) {
+            onClick()
         }
     }
 }
