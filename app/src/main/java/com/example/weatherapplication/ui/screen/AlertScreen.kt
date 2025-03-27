c925ea159416a0504c05b764f560556505396a9f
Nouragami7 <https://github.com/Nouragami7>
2025-03-27 18:16:48 +0200

    Design alert screen

--- app/src/main/java/com/example/weatherapplication/ui/screen/AlertScreen.kt
+++ app/src/main/java/com/example/weatherapplication/ui/screen/AlertScreen.kt
@@ -1,15 +1,21 @@
 package com.example.weatherapplication.ui.screen
 
+import LottieAnimationView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,19 +30,21 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.weatherapplication.R
 import com.example.weatherapplication.ui.theme.SkyBlue
 import com.example.weatherapplication.ui.theme.onPrimaryDark
 import com.example.weatherapplication.ui.theme.primaryContainerDark
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AlertScreen(){
-    var showAlert by remember { mutableStateOf(false) }
+fun AlertScreen() {
+    var isSheetOpen by remember { mutableStateOf(false) }
+    var hasData by remember { mutableStateOf(false) } // Change this dynamically based on your data
 
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
-                onClick = {
-                },
+                onClick = { isSheetOpen = true },
                 modifier = Modifier.padding(16.dp, bottom = 60.dp),
                 containerColor = SkyBlue
             ) {
@@ -55,32 +63,71 @@ fun AlertScreen(){
                 .padding(paddingValues),
             contentAlignment = Alignment.Center
         ) {
-            if (showAlert) {
-            } else {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-
-                    ) {
-                    Text(
-                        text = "Saved Locations",
-                        style = TextStyle(
-                            brush = Brush.verticalGradient(
-                                0f to primaryContainerDark,
-                                1f to onPrimaryDark
-                            ),
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Black
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Saved Alert",
+                    style = TextStyle(
+                        brush = Brush.verticalGradient(
+                            0f to primaryContainerDark,
+                            1f to onPrimaryDark
                         ),
-                        color = onPrimaryDark,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(top = 16.dp)
-                    )
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Black
+                    ),
+                    color = onPrimaryDark,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(top = 16.dp)
+                )
 
-                    Spacer(modifier = Modifier.weight(1f))
+                Spacer(modifier = Modifier.weight(1f))
 
+                if (!hasData) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        LottieAnimationView(
+                            resId = R.raw.no_fav,
+                            modifier = Modifier.size(300.dp)
+                        )
                     }
                 }
+
+            }
+        }
+
+        if (isSheetOpen) {
+            ModalBottomSheet(
+                onDismissRequest = { isSheetOpen = false },
+            ) {
+                BottomSheetContent { isSheetOpen = false }
             }
         }
     }
+}
+
+@Composable
+fun BottomSheetContent(onDismiss: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Set an Alert",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(onClick = onDismiss) {
+            Text("Close")
+        }
+    }
+}
